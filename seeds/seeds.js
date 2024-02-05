09e87bfc52af78b1e2d22aaf580f1c6d14446a11
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-02-05 12:33:49 -0800

    redeploy

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,10 +1,16 @@
+const { Sequelize } = require('sequelize');
+
 const seedErrorReports = require('./ErrorReport-seeds');
 const seedErrorSolutions = require('./ErrorSolution-seeds');
 const seedRecipes = require('./Recipe-seeds');
 const seedSteps = require('./Step-seeds');
 const seedUsers = require('./User-seeds');
 
-const sequelize = require('../config/connection');
+// seed system 
+
+const seedProduction = true; // seeding production or local
+
+const sequelize = seedProduction ? new Sequelize('mysql://df8ug3vp7hnu1lb0:xq4eafk9yfpo55lf@s0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/czhtlxrp3s718ti9'): require('../config/connection');
 
 async function seedAll() {
     await sequelize.sync({ force: true });
