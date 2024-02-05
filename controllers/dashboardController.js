2376164ee113ce705e9bb045a3bba7480e7082ab
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-02-05 12:54:39 -0800

    heroku deploy

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,4 +1,7 @@
 const recipeController = require('./recipeController');
+
+const { Recipe, User } = require('../models')
+
 const dashboardController = {
     async showDashBoardPage(req, res) {
         const recipesData = await recipeController.getUserRecipes(req, res);
@@ -24,16 +27,25 @@ const dashboardController = {
         });
     },
     async showAllRecipes(req, res) {
-        const recipesData = await recipeController.getAllRecipes(req, res);
-        const recipes = recipesData.recipes;
-        res.render('pages/allRecipesPage' , {
-            title: 'All Recipes',
-            header: 'All Recipes',
-            userId: req.session.userId,
-            userName: req.session.userName,
-            recipes: recipes,
-            errors: req.errors
-        });
+        try{ // 
+            const recipesData = await Recipe.findAll({
+                include: User
+            })
+            // if single obj get 
+            // if array map over and get plain true for each
+            // const recipes = recipesData.recipes;
+            res.render('pages/allRecipesPage' , {
+                title: 'All Recipes',
+                header: 'All Recipes',
+                userId: req.session.userId,
+                userName: req.session.userName,
+                recipes: recipesData && recipesData.map(r => r.get({ plain: true})),
+                errors: req.errors
+            });
+        } catch(err){
+            console.log(err)
+        }
+
      },
      async newRecipe(req, res) {
         res.render('pages/newRecipePage', {
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -8,11 +8,17 @@ const seedUsers = require('./User-seeds');
 
 // seed system 
 
-const seedProduction = true; // seeding production or local
+const seedProduction = false; // seeding production or local
 
 const sequelize = seedProduction ? new Sequelize('mysql://df8ug3vp7hnu1lb0:xq4eafk9yfpo55lf@s0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/czhtlxrp3s718ti9'): require('../config/connection');
 
 async function seedAll() {
+    // if(!seedProduction){
+    //     await sequelize.query('DROP DATABASE install_lasagna_db');
+    //     await sequelize.query('CREATE DATABASE install_lasagna_db');
+    //     await sequelize.query('USE install_lasagna_db');
+    
+    // }
     await sequelize.sync({ force: true });
 
     await seedUsers();
